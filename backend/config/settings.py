1bce877ca5639ae3617ff27ae639eba586249edc
Utilisateur <user@example.com>
2026-04-15 19:46:34 +0200

    Déplacement railway.json à la racine

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -48,7 +48,7 @@ TEMPLATES=[
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=f'sqlite:///{os.path.join(BASE_DIR, "db.sqlite3")}'
+        default=f'sqlite:///{BASE_DIR}/db.sqlite3'
     )
 }
 
